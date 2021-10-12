ea47f9768211b1699e0e930098df207555a5e95b
Jakub <kuba1361@onet.eu>
2021-10-12 21:23:46 +0200

    add(single article in recycler)

--- app/src/main/java/com/example/spaceflightnews/MainActivity.kt
+++ app/src/main/java/com/example/spaceflightnews/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.spaceflightnews
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.spaceflightnews.adapters.ArticlesAdapter
+import com.example.spaceflightnews.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var recyclerAdapter: ArticlesAdapter
 
     private val viewModel: MainViewModel by lazy {
         ViewModelProvider(this).get(MainViewModel::class.java)
@@ -13,14 +19,24 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        viewModel.articles.observe(this){
+        recyclerAdapter = ArticlesAdapter {
+
+        }
+
+        binding.articlesRecyclerView.adapter = recyclerAdapter
+        binding.articlesRecyclerView.layoutManager = LinearLayoutManager(this)
+
+        viewModel.articles.observe(this) {
+            // TODO if != null itp
+            recyclerAdapter.submitList(it.data)
             Log.v(TAG, it.data.toString())
         }
     }
 
-    companion object{
+    companion object {
         private const val TAG = "MainActivity"
     }
 }
--- app/src/main/java/com/example/spaceflightnews/adapters/ArticlesAdapter.kt
+++ app/src/main/java/com/example/spaceflightnews/adapters/ArticlesAdapter.kt
@@ -1,33 +1,63 @@
 package com.example.spaceflightnews.adapters
 
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import coil.size.Scale
+import coil.transform.RoundedCornersTransformation
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.FitCenter
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
 import com.example.spaceflightnews.databinding.SingleArticleBinding
 import com.example.spaceflightnews.model.Article
 
-class ArticlesAdapter (private val onClick: (Article) -> Unit) :
-ListAdapter<Article, ArticlesAdapter.ViewHolder>(DiffCallback) {
+class ArticlesAdapter(private val onClick: (Article) -> Unit) :
+    ListAdapter<Article, ArticlesAdapter.ViewHolder>(DiffCallback) {
 
     // TODO kolejność
     inner class ViewHolder(
         private val binding: SingleArticleBinding,
         private val onClick: (Article) -> Unit,
     ) :
-        RecyclerView.ViewHolder(binding.root){
+        RecyclerView.ViewHolder(binding.root) {
 
+        fun bind(article: Article) {
+            with(binding){
+                title.text = article.title
+
+                if(article.summary.length >= 255){
+                    description.visibility = View.GONE
+                }else{
+                    description.visibility = View.VISIBLE
+                    description.text = article.summary
+                }
+                Glide.with(root)
+                    .load(article.imageUrl)
+                    .transform(RoundedCorners(36), FitCenter())
+                    .into(image)
+            }
         }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        val binding =
+            SingleArticleBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding, onClick)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bind(getItem(position))
+    }
+
+    companion object {
+        private const val MAX_DESCRIPTION_LENGTH = 255
     }
 }
-
 
 
 // TODO
