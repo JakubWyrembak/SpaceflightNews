fd75970355406bfad90334864a2401b7b78d2d4b
Jakub <kuba1361@onet.eu>
2021-10-13 14:11:23 +0200

    add(Splash screen)

---
+++ app/src/main/java/com/example/spaceflightnews/SplashActivity.kt
@@ -0,0 +1,59 @@
+package com.example.spaceflightnews
+
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowInsets
+import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
+import com.example.spaceflightnews.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        hideBar()
+        animate()
+    }
+
+    private fun hideBar() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            @Suppress("DEPRECATION")
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            )
+        }
+    }
+
+    private fun animate() {
+        val splashAnimation = AnimationUtils.loadAnimation(this, R.anim.anim_splash)
+        splashAnimation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {
+            }
+
+            // TODO coroutines?
+            override fun onAnimationEnd(animation: Animation?) {
+                Handler(Looper.getMainLooper()).postDelayed({
+                    startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                    finish()
+                }, 1000)
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {}
+        })
+
+        binding.rocketIcon.animation = splashAnimation
+    }
+}
